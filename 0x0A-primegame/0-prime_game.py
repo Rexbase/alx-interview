ed44290ef44431d1cd07b3c4f565b70fd0a4688c
Rexbase <gdnwstom@gmail.com>
2024-05-16 11:10:30 -0700

    Updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,106 +1,45 @@
-
 #!/usr/bin/python3
-"""
-Module that contains various functions that
-allow to play the Prime Game
-"""
+
+""" Task 0. Prime Game """
 
 
-def bsd_rand(seed):
-    """
-    decorator for randomizing
-    """
-    def rand():
-        """
-        the seed
-        """
-        nonlocal seed
-        seed = (1103515245*seed + 12345) & 0x7fffffff
-        return seed
-    return rand
-
-
-rand = bsd_rand(32)
-
-
-def isWinner(x, nums):
-    """
-    Return winner from  the game
-    """
-    if x <= 0 or x > 10000:
-        return None
-    players_dict = {'Maria': 0, 'Ben': 0}
-    players = ['Maria', 'Ben']
-    players_dict_winner = {'Maria': 0, 'Ben': 0}
-    if x == len(nums):
-        for number in nums:
-            consecutive_int = [i for i in range(1, number + 1)]
-            recursive_play(players[:], players_dict, consecutive_int)
-            maria_wins = players_dict['Maria']
-            ben_wins = players_dict['Ben']
-            if maria_wins == 1:
-                players_dict_winner['Maria'] += 1
-            if ben_wins == 1:
-                players_dict_winner['Ben'] += 1
-    else:
-        while x != 0:
-            for number in nums:
-                consecutive_int = [i for i in range(1, number + 1)]
-                recursive_play(players[:], players_dict, consecutive_int)
-                maria_wins = players_dict['Maria']
-                ben_wins = players_dict['Ben']
-                if maria_wins == 1:
-                    players_dict_winner['Maria'] += 1
-                if ben_wins == 1:
-                    players_dict_winner['Ben'] += 1
-            x -= 1
-    if players_dict_winner['Maria'] == players_dict_winner['Ben']:
-        winner = None
-    elif players_dict_winner['Maria'] > players_dict_winner['Ben']:
-        winner = 'Maria'
-    else:
-        winner = 'Ben'
-    return winner
-
-
-def recursive_play(players, players_dict, consecutive_int):
-    """
-    recursive helper function on the game
-    """
-    if len(consecutive_int) == 0:
-        return
-    toggle_player(players, players_dict)
-    prime_numbers = []
-    for number in consecutive_int:
-        if check_prime(number):
-            prime_numbers.append(number)
-    if len(prime_numbers) == 0:
-        toggle_player(players, players_dict)
-        return
-    prime_pick = prime_numbers[rand() % len(prime_numbers)]
-    consecutive_int.remove(prime_pick)
-    for number in consecutive_int[:]:
-        if number % prime_pick == 0 and number in consecutive_int:
-            consecutive_int.remove(number)
-    recursive_play(players, players_dict, consecutive_int)
-
-
-def check_prime(number):
-    """check's if number is prime or not"""
-    if number == 1:
+def is_prime(number):
+    """Checks if a number is prime or not"""
+    if number < 2:
         return False
-    primes = [2, 3, 5, 7]
-    for p in primes:
-        if number % p == 0 and p != number:
+    for i in range(2, int(number**1/2) + 1):
+        if number % i == 0:
             return False
     return True
 
 
-def toggle_player(players, players_dict):
-    """
-    toggle current player
-    """
-    player = players.pop(0)
-    players_dict[player] = 1
-    players_dict[players[-1]] = 0
-    players.append(player)
+def maria_vs_ben(n):
+    """Determines if Maria or Ben wins after a turn"""
+    prime_count = 0
+    for i in range(2, n + 1):
+        if is_prime(i):
+            prime_count += 1
+    return prime_count % 2 == 1
+
+
+def isWinner(x, nums):
+    """Returns the name of the player that won the most rounds"""
+
+    if x < 1 or x >= 10000 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if maria_vs_ben(n):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
