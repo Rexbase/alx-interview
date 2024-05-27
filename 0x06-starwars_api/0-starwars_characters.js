c7d11575881019f342961398e6c61f54141dc456
Rexbase <gdnwstom@gmail.com>
2024-05-27 05:42:27 -0700

    Updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,42 @@
 #!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+const request = require('request');
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+/**
+ * Returns the name of a Star Wars character given their URL
+ * @param {string} characterUrl - The URL of the character
+ * @returns {Promise<string>} - A promise that resolves to the character's name
+ */
+function getName (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
 }
 
-starwarsCharacters(filmID);
+/**
+ * Displays the names of all characters in a Star Wars film
+ * @returns {Promise<void>} - A promise that resolves when all names have been displayed
+ */
+const displayData = async () => {
+  try {
+    const filmId = process.argv[2];
+    request.get(`https://swapi-api.hbtn.io/api/films/${filmId}`, async (error, response, body) => {
+      if (error) {
+        console.log('Found error');
+      } else {
+        const names = await Promise.all(JSON.parse(body).characters.map(character => getName(character)));
+        names.forEach(name => console.log(name));
+      }
+    });
+  } catch (err) {
+    console.log('Found error');
+  }
+};
+
+displayData();
